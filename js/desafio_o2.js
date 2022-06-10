11f5177641abb68974f9f35fc6de70b8f7018d6d
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2022-06-10 16:24:33 -0300

    add desafios 01 02

---
+++ js/desafio_o2.js
@@ -0,0 +1,16 @@
+const nome = prompt("Digite seu nome");
+const idade = prompt("Digite sua idade");
+const estudos = prompt("Qual linguagem de Programação voce esta estudando");
+
+const msg = `Olá ${nome}, voce tem ${idade} e já está aprendendo ${estudos} ! `;
+
+alert(msg);
+
+const nome1 = prompt("Digite seu nome");
+const msg1 = `Olá ${nome1}`;
+
+alert(msg1);
+
+
+
+
